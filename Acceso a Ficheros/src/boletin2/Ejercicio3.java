a75bf8ee0052878ae977a6ad0428ec90743b1c23
usuario <usuario@DESKTOP-68H0REC>
2020-05-26 12:31:45 +0200

    .

--- Acceso a Ficheros/src/boletin2/Ejercicio3.java
+++ Acceso a Ficheros/src/boletin2/Ejercicio3.java
@@ -24,42 +24,27 @@ public class Ejercicio3 {
 		Ejercicio1.crearFichero(directorio2, archivo2);
 		
 		//escribir en el fichero parcelas2.txt, la cadena creada desde el ArrayList
-		String cadena ;
+		String cadena ="";
 		
-		FileWriter fw;
-		try {
-			
-			String path = directorio2+archivo2;
-			fw = new FileWriter(path);
-			
-			for (int i=1; i == (parcelas.size() - 1); i++ ) {
-				
+		System.out.println("contenido de parcela : " + parcelas.size());		
+		
+		for (int i=0; i < parcelas.size(); i++ ) {
+				System.out.println("contenido de parcela : " + parcelas.get(i));		
 				Parcela par = parcelas.get(i);
-				cadena = par.getCalle()+"-"+
+				
+				
+				cadena = cadena + par.getCalle()+"-"+
 						par.getNumero()+"-"+
 						par.getPoblacion()+"-"+
 						par.getProvincia()+"-"+
 						par.getMetrosCuadrados()+";";
-				
-				
-			}
-			
-			
-			fw.flush();
-			fw.close();
-		
-		} catch (FileNotFoundException ex) {
-			System.out.printf("HA ocurrido un error. No se ha encontrado fichero: %s ",ex.getMessage());
-			
-		} catch (IOException ex){
-			System.out.printf("Ha ocurrido una excepcion indeterminada: %s", ex.getMessage());
 			
 		}
-		
-		
-		
+	
+		System.out.println("contenido de cadena : " + cadena);	
+			
 		Ejercicio1.escribirFichero(cadena, directorio2, archivo2);
-				
+
 				
 		//leer el contenido del fichero y mostrarlo por pantalla
 		Ejercicio1.leerFichero(directorio2, archivo2);
